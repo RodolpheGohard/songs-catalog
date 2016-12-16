9159c1fa9e1e816131d7a370237d3477bb005078
Rodolphe Gohard <rodolphe@gohard.fr>
2016-12-16 11:35:13 +0100

    fix add song input

--- index.elm
+++ index.elm
@@ -6,32 +6,37 @@ import String
 main =
     Html.beginnerProgram { model = model, view = view, update = update }
     
-type alias Model =
-    List String
 
 type Msg
-  = Change String | Tg
+    = Change String | AddSong
+
+
+type alias Model
+    = {
+        songs: List String,
+        inputSong: String
+    }
 
 model : Model
-model =
-    ["Cantaloupe Island"]
+model = Model ["Cantaloupe Island"] ""
+    
 
 view : Model -> Html Msg
 view model =
     div []
         [
-            div [] [ text <| String.join "" model],
-            div [] (List.map (\songName -> text songName) model),
-            input [placeholder "Text placeholder", onInput Change] [],
-            button [onClick Tg] [text "add"]
+            div [] [ text <| String.join "" model.songs],
+            div [] (List.map (\songName -> text songName) model.songs),
+            input [value model.inputSong, placeholder "Text placeholder", onInput Change] [],
+            button [onClick AddSong] [text "add"]
         ]
         
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Tg -> 
-            "ta soeur" :: model
+        AddSong -> 
+            Model (model.inputSong :: model.songs) ""
         Change newContent ->
-            newContent :: model
+            { model | inputSong = newContent }
 
 
